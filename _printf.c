6a970c64eca73d075fe8774e1d644b941e9aeb9b
AmonMunyai <Amonmunyai11@gmail.com>
2022-11-20 18:17:04 +0200

    added % and NULL guards

--- _printf.c
+++ _printf.c
@@ -16,25 +16,35 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 
+	if (format[0] == '%' && format[1] == '\0') /* % only */
+		return (-1);
+
+	if (format[0] == '%' && format[1] == ' ' && format[2] == '\0')
+		return (-1);
+
 	va_start(args, format);
 
 	for (index = 0, t_length = 0; format[index] != '\0'; index++)
 	{
-		if (format[index] != '%')
-			t_length += _putchar(format[index]); /* write to stdout */
-		else
+		if (format[index] == '%')
 		{
 			index++;
 			if (format[index] == '\0') /* % at end of string format */
 				break;
 
+			if (format[index] == '%')
+			{
+				t_length += _putchar('%');
+				continue;
+			}
+
 			f = get_p(format[index]);
 
-			if (f == NULL) /* handle invalid id's */
-				t_length += _putchar('%');
-			else
-				t_length += f(args);
+			t_length += (f == NULL) ?
+				_printf("%%%c", format[index]) : f(args);
 		}
+		else
+			t_length += _putchar(format[index]); /* write to stdout */
 	}
 	va_end(args);
 
