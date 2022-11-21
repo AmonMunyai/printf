0810b5811b7ce6b4869dcbcd586117a41073b348
belovedtoluwanimi <belovedtoluwanimi16@gmail.com>
2022-11-21 01:44:16 -0800

    added % and NULL guards

--- _printf.c
+++ _printf.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
  * _printf - writes output stdout according to a format
  * @format: output to write
@@ -51,17 +49,4 @@ int _printf(const char *format, ...)
 	va_end(args);
 
 	return (t_length);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
 }
